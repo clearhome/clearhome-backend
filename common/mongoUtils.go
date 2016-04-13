a12d9903108d95e9624eb8beb328f529e9966143
Bill Broughton <wlbrough@gmail.com>
2016-04-12 21:42:48 -0400

    Working on MongoDB connector

---
+++ common/mongoUtils.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+  "gopkg.in/mgo.v2"
+)
+
+var session *mgo.Session
+
+func GetSession() *mgo.Session {
+  if session == nil {
+    var err error
+
+    session, err = mgo.DialWithInfo(&mgo.DialInfo{
+      Addrs:    []string{AppConfig.MongoDBHost},
+      Username: AppConfig.DBUser,
+      Password: AppConfig.DBPwd,
+      Timeout:  60 * time.Second,
+    })
+    if err != nil {
+      log.Fatalf("[GetSession]: %s\n", err)
+    }
+  }
+}
